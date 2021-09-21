d8fdc22dcc2c7ddd05bc6acad73b0a6ae23e12ed
Diwen Liu <liud7879@gmail.com>
2021-09-20 22:17:08 -0400

    Init Successful

--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -1,33 +1,39 @@
 import DeckGL from '@deck.gl/react';
-import {GoogleMapsOverlay} from '@deck.gl/google-maps';
+// import {GoogleMapsOverlay} from '@deck.gl/google-maps';
 import {LineLayer} from '@deck.gl/layers';
 import {StaticMap} from 'react-map-gl';
 
+
 const HomeScreen = (props) =>{
-    const [viewport, setViewport] = useState({
-        latitude: 37.7577,
-        longitude: -122.4376,
-        zoom: 8
-    });
+   // Set your mapbox access token here
+    const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoiZGl3bGl1IiwiYSI6ImNrdHQ1M3hjdTFuZWcycXBxczAyYnRud3EifQ.WUk5cILDRQQNOaae60Hb9A";
     
-    
+    // Viewport settings
+    const INITIAL_VIEW_STATE = {
+        longitude: -122.41669,
+        latitude: 37.7853,
+        zoom: 13,
+        pitch: 0,
+        bearing: 0
+    };
+
+    // Data to be used by the LineLayer
     const data = [
         {sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}
     ];
-    
-    // const layers = [
-    //     new LineLayer({id: 'line-layer', data})
-    // ];
+
+    const layers = [
+        new LineLayer({id: 'line-layer', data})
+    ];
 
     return (
         <DeckGL
-            initialViewState={INITIAL_VIEW_STATE}
-            controller={true}
-            layers={layers}
+          initialViewState={INITIAL_VIEW_STATE}
+          controller={true}
+          layers={layers}
         >
-            <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+          <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
         </DeckGL>
-        
-    )
+      );
 }
 export default HomeScreen;
