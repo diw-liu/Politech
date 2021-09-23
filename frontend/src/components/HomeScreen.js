43f741094711a591b76e9ba9d3b6492a8492eee4
Diwen Liu <liud7879@gmail.com>
2021-09-22 20:18:38 -0400

    Preccint work

--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -1,8 +1,11 @@
 import DeckGL from '@deck.gl/react';
-// import {GoogleMapsOverlay} from '@deck.gl/google-maps';
-import {LineLayer} from '@deck.gl/layers';
+import {GeoJsonLayer} from '@deck.gl/layers';
+import {PolygonLayer} from '@deck.gl/layers';
 import {StaticMap} from 'react-map-gl';
 
+let data = require('../maryland.json');
+
+// console.log(data, 'the json obj');
 
 const HomeScreen = (props) =>{
    // Set your mapbox access token here
@@ -18,19 +21,91 @@ const HomeScreen = (props) =>{
     };
 
     // Data to be used by the LineLayer
-    const data = [
-        {sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}
-    ];
+    // const data = [
+    //     {sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}
+    // ];
 
-    const layers = [
-        new LineLayer({id: 'line-layer', data})
-    ];
+    const Base = new GeoJsonLayer({
+      id: 'geojson-layer',
+      data,
+      pickable: true,
+      stroked: false,
+      filled: true,
+      extruded: true,
+      pointType: 'circle',
+      lineWidthScale: 20,
+      lineWidthMinPixels: 2,
+      getFillColor: [160, 160, 180, 200],
+      getLineColor: [80, 80, 80],
+      getPointRadius: 100,
+      getLineWidth: 1,
+      getElevation: 30
+    });
+    console.log(Base);
+
+    const preccint = []
+
+    // data["features"].forEach(element => {
+    //   preccint.push(new GeoJsonLayer({
+    //     id: 'geojson-layer',
+    //     data: element,
+    //     pickable: true,
+    //     stroked: false,
+    //     filled: true,
+    //     extruded: true,
+    //     pointType: 'circle',
+    //     lineWidthScale: 20,
+    //     lineWidthMinPixels: 2,
+    //   }))
+    // })
+   
+    // preccint.push(new GeoJsonLayer({
+    //   id: 'geojson-layer',
+    //   data: data['features'][0],
+    //   pickable: true,
+    //   stroked: false,
+    //   filled: true,
+    //   extruded: true,
+    //   pointType: 'circle',
+    //   lineWidthScale: 20,
+    //   lineWidthMinPixels: 2,
+    // }))
+    // preccint.push(new GeoJsonLayer({
+    //   id: 'geojson-layer',
+    //   data: data['features'][100],
+    //   pickable: true,
+    //   stroked: false,
+    //   filled: true,
+    //   extruded: false,
+    //   pointType: 'circle',
+    //   lineWidthScale: 20,
+    //   lineWidthMinPixels: 2,
+    //   getFillColor: [160, 160, 180, 200],
+    //   getLineColor: [80, 80, 80],
+    // }))
+
+    console.log(preccint);
+    // const layer = new PolygonLayer({
+    //   id: 'polygon-layer',
+    //   data,
+    //   pickable: true,
+    //   stroked: true,
+    //   filled: true,
+    //   wireframe: true,
+    //   lineWidthMinPixels: 1,
+    //   getPolygon: d => d.contour,
+    //   getElevation: d => d.population / d.area / 10,
+    //   getFillColor: d => [d.population / d.area / 60, 140, 0],
+    //   getLineColor: [80, 80, 80],
+    //   getLineWidth: 1
+    // });
 
     return (
         <DeckGL
           initialViewState={INITIAL_VIEW_STATE}
           controller={true}
-          layers={layers}
+          layers={Base}
+          getTooltip={({object}) => object && (object.properties.name || object.properties.station)}
         >
           <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
         </DeckGL>
