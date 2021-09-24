b7ec1381e80144496deb578b247c079443c7679f
jeffzhong7 <jeffzhong7@gmail.com>
2021-09-24 16:29:57 -0400

    Changed the dropdown

--- frontend/src/components/Dropdown.js
+++ frontend/src/components/Dropdown.js
@@ -1,20 +1,26 @@
 import React, { useState, useEffect } from 'react';
+import '../css/StateDropdown.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 export default function Dropdown() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  // Similar to componentDidMount and componentDidUpdate:
-  useEffect(() => {
-    // Update the document title using the browser API
-    document.title = `You clicked ${count} times`;
-  });
+  // // Similar to componentDidMount and componentDidUpdate:
+  // useEffect(() => {
+  //   // Update the document title using the browser API
+  //   document.title = `You clicked ${count} times`;
+  // });
 
   return (
-    <div style={{zIndex:'1', position: 'absolute'}}>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+    <div className='StateDropdown'>
+      State
+      <hr/>
+      <select>
+        <option value='maryland'>Maryland</option>
+        <option value='michigan'>Michigan</option>
+        <option value='pennsylvania'>Pennsylvania</option>
+      </select>
     </div>
   );
 }
