90016fc35bd95889106bd4eb000839b53b92c31a
ZhenglongQian135 <61164795+ZhenglongQian135@users.noreply.github.com>
2021-11-13 17:21:27 -0500

    Update CensusBlock.java

--- backend/src/main/java/com/example/demo/model/CensusBlock.java
+++ backend/src/main/java/com/example/demo/model/CensusBlock.java
@@ -1,1 +1,31 @@
 package com.example.demo.model;
+
+import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+public class CensusBlock {
+    private int censusBlockId;
+    private Demographics demographics;
+    private District parentDistrict;
+    private List<Election> elections;
+    private Precinct precinct;
+    private Polygon blockGeometry;
+    private Boolean isBorder;
+
+    @Id
+    @GeneratedValue
+    public int getCensusBlockId() {
+        return censusBlockId;
+    }
+
+    public void setCensusBlockId(int censusBlockId) {
+        this.censusBlockId = censusBlockId;
+    }
+
+    //TODO
+    public CensusBlock findDistrictNeighbor(District d){
+
+    }
+
+}
