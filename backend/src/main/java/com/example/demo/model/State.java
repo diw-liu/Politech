997dda8b67beacacb09e3b45dc76262554cb441d
ZhenglongQian135 <61164795+ZhenglongQian135@users.noreply.github.com>
2021-11-13 17:26:10 -0500

    update model

--- backend/src/main/java/com/example/demo/model/State.java
+++ backend/src/main/java/com/example/demo/model/State.java
@@ -1,2 +1,19 @@
 package com.example.demo.model;
+import com.example.demo.model.data.*;
 
+import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class State {
+    String name;
+    Districting enactedDistricting;
+    Districting currentDistricting;
+    Districting redistrictedDistricting;
+    List<Districting> districtings;
+    StateOutline stateOutline;
+    Demographic demographic;
+    List<Election> elections;
+    BoxAndWhisker[] boxAndWhiskerBatch;
+}
---
+++ backend/src/main/java/com/example/demo/model/data/BoxAndWhisker.java
@@ -0,0 +1,5 @@
+package com.example.demo.model.data;
+
+public class BoxAndWhisker{
+
+}
