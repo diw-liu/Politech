94e89e01aec38cfd0c3b024048c5d6de187d880e
stevenhsin <steven.hsin@stonybrook.edu>
2021-12-14 13:48:28 -0500

    reorganized left table

--- frontend/src/components/LeftBar/PlanStatistics.js
+++ frontend/src/components/LeftBar/PlanStatistics.js
@@ -5,37 +5,30 @@ import Plans from '../../data/mockGenerated.js';
 const PlanStatistics = (props) => {
   var plans = Plans;
 
+  console.log("TETETTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT");
   console.log(props.plan);
   // console.log(props.plan);
 
     return(
         <div>
-          <table style={{ width: '100%' }}>
+          {props.plan == 0 ? <div>  </div> : <table style={{ width: '100%' }}>
             <tr>
-              <td> Population Equality (%) </td>
-              <td style={{ textAlign: 'right' }}> {(props.plan == 0) ? 83 : plans[props.plan].pop_eq} </td> 
+              <td> Objective Function </td>
+              <td style={{ textAlign: 'right' }}> {plans[props.plan].objectiveFunction} </td> 
             </tr> 
             <tr>
-              <td> Racial Deviation (%) </td>
-              <td style={{ textAlign: 'right' }}> {(props.plan == 0) ? 87 :plans[props.plan].dev} </td> 
+              <td> Opportunity Districts </td>
+              <td style={{ textAlign: 'right' }}> {plans[props.plan].opportunityDistricts} </td> 
             </tr> 
             <tr>
-              <td> Majority-Minority districts </td>
-              <td style={{ textAlign: 'right' }}> {(props.plan == 0) ? 3 :plans[props.plan].maj_min} </td> 
+              <td> Compactness Measure </td>
+              <td style={{ textAlign: 'right' }}> {plans[props.plan].polsbyPopper } </td> 
             </tr> 
             <tr>
-              <td> Efficiency Gap </td>
-              <td style={{ textAlign: 'right' }}> {(props.plan == 0) ? 77 :plans[props.plan].eff_gap } </td> 
+              <td> Population Equality </td>
+              <td style={{ textAlign: 'right' }}> {plans[props.plan].populationEquality} </td> 
             </tr> 
-            <tr>
-              <td> Partisan Symmetry </td>
-              <td style={{ textAlign: 'right' }}> {(props.plan == 0) ? 11 :plans[props.plan].symmetry} </td> 
-            </tr> 
-            <tr>
-              <td> Graph Compactness </td>
-              <td style={{ textAlign: 'right' }}> {(props.plan == 0) ? 13 :plans[props.plan].compactness} </td> 
-            </tr> 
-          </table>
+          </table>}
         </div>
     );
     
