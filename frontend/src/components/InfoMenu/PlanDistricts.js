f61e75da252ec861912cac314b31373e4d99e3f8
jeffzhong7 <jeffzhong7@gmail.com>
2021-12-16 11:43:39 -0500

    Add district list for plans

--- frontend/src/components/InfoMenu/PlanDistricts.js
+++ frontend/src/components/InfoMenu/PlanDistricts.js
@@ -1,24 +1,41 @@
 import React, { useState } from 'react';
 
-const ElectionEnum = {
-    democraticVotes: 'Democratic',
-    republicanVotes: 'Republican',
-    otherVotes: 'Other'
-}
-
-const PopulationEnum = {
-    white: 'White',
-    hispanic: 'Hispanic',
-    black: 'African American',
-    asian: 'Asian American',
-    nativ: 'Native American',
-    other: 'Other',
-    total: 'Total'
-}
 
 const PlanDistricts = (props) => {
-    return(
-        <div></div>
+    var typeToUse = "";
+    if (props.popType == 0) {
+        typeToUse = "population"
+    } else {
+        typeToUse = "vap"
+    }
+    console.log(props.districtings)
+    console.log(props.plan)
+
+    return (
+        <div>   
+            <div>
+                <table style={{ width: '100%' }}>
+                    <tr>
+                        <th>District</th>
+                        <th style={{ textAlign: 'right' }}>Population</th>
+                        <th style={{ textAlign: 'right' }}>Vote</th>
+                        <th style={{ textAlign: 'right' }}>Democratic</th>
+                        <th style={{ textAlign: 'right' }}>Republican</th>
+                    </tr>
+                    {
+                        props.districtings.find(p => p.id === props.plan).districts.map(x => (
+                            <tr key={x.cd} style={{ textAlign: 'right' }} align="start">
+                                <td className="number" style={{ width: '15%' }}>{x.cd}</td>
+                                <td className="population">{x[typeToUse].total.toLocaleString()}</td>
+                                <td className="totalVote">{x.election.totalVotes.toLocaleString()}</td>
+                                <td className="demVote">{x.election.democraticVotes.toLocaleString()}</td>
+                                <td className="repVote">{x.election.republicanVotes.toLocaleString()}</td>
+                            </tr>
+                        ))   
+                    }
+                </table>
+            </div>
+        </div>
     )   
 }
 export default PlanDistricts
