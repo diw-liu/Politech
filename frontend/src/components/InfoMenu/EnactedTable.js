0cc026181a3ca71609228594d3417a9d8b467327
jeffzhong7 <jeffzhong7@gmail.com>
2021-12-16 12:15:32 -0500

    Added tooltip for more detailed population

--- frontend/src/components/InfoMenu/EnactedTable.js
+++ frontend/src/components/InfoMenu/EnactedTable.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ReactTooltip from 'react-tooltip';
 
 import districts from '../../data/mock.js';
 
@@ -25,13 +26,23 @@ const EnactedTable = (props) => {
                 </tr>
                 {
                     props.enactedInfo.districts.map(x => (
-                        <tr key={x.cd} style={{ textAlign: 'right' }} align="start">
-                            <td className="number" style={{ width: '15%' }}>{x.cd}</td>
-                            <td className="population">{x[typeToUse].total.toLocaleString()}</td>
-                            <td className="totalVote">{x.election.totalVotes.toLocaleString()}</td>
-                            <td className="demVote">{x.election.democraticVotes.toLocaleString()}</td>
-                            <td className="repVote">{x.election.republicanVotes.toLocaleString()}</td>
-                        </tr>
+                        <>
+                            <ReactTooltip id={x.cd} place="bottom" effect="solid">
+                                <p> White: {x[typeToUse].white.toLocaleString()} </p>
+                                <p> Black: {x[typeToUse].black.toLocaleString()} </p> 
+                                <p> Hispanic: {x[typeToUse].hispanic.toLocaleString()} </p> 
+                                <p> Native American: {x[typeToUse].nativ.toLocaleString()} </p> 
+                                <p> Asian: {x[typeToUse].asian.toLocaleString()} </p> 
+                                <p> Other: {x[typeToUse].other.toLocaleString()} </p> 
+                            </ReactTooltip>
+                            <tr key={x.cd} style={{ textAlign: 'right' }} align="start">
+                                <td className="number" style={{ width: '15%' }}>{x.cd}</td>
+                                <td className="population" data-tip data-for={x.cd}>{x[typeToUse].total.toLocaleString()}</td>
+                                <td className="totalVote">{x.election.totalVotes.toLocaleString()}</td>
+                                <td className="demVote">{x.election.democraticVotes.toLocaleString()}</td>
+                                <td className="repVote">{x.election.republicanVotes.toLocaleString()}</td>
+                            </tr>
+                        </>
                     ))   
                 }
             </table>
