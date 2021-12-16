5fecdb260edfe84c2d5df62b75e5f79c01090307
stevenhsin <steven.hsin@stonybrook.edu>
2021-12-16 05:35:02 -0500

    geting enacted points

--- frontend/src/components/InfoMenu/BoxAndWhisker.js
+++ frontend/src/components/InfoMenu/BoxAndWhisker.js
@@ -10,23 +10,33 @@ const basisMap = {
         "REPUBLICAN" : "Republican"
 }
 
+const REVERSE = {
+    "African American" : "BLACK",
+    "Democratic" : "DEMOCRATIC",
+    "Republican" : "REPUBLICAN"
+}
+
+const KEYS = {
+    "MD" : "24",
+    "MI" : "26",
+    "PA" : "42"
+}
+
 const BoxAndWhisker = (props) => {
 
     const [basis, setBasis] = useState(basisMap["BLACK"]);
     const [ensemble, setEnsemble] = useState([]);
+    const [enactedData, setEnactedData] = useState([]);
     const [enactedPoints, setEnactedPoints] = useState([]);
-    const [selectedPointsAll, setSelectedPointsAll] = useState({"recombination_of_districts-0": []});
+    // const [selectedPointsAll, setSelectedPointsAll] = useState({"recombination_of_districts-0": []});
     const [boxes, setBoxes] = useState(props.plots.filter( single => single.basis == 'BLACK').sort((a, b) => (a.min > b.min) ? 1 : -1));
 
-
-    // console.log(plots);
-    // console.log(plots[0].min)
-    const handleBasisSelect = (key) => {
-        setBasis(basisMap[key]);
-        setBoxes(props.plots.filter(single => single.basis == key).sort((a, b) => (a.max > b.max) ? 1 : -1));
-        console.log(boxes);
+    // const [enactedData,setEnactedData] = useState([]);
+    const getEnactedData=()=>{
+        var data = require('../../data/' + props.stateName + "/" + REVERSE[basis] + "/" + KEYS[props.stateName] + "PL0.json");
+        setEnactedData(Object.values(data).sort((a, b) => (a < b) ? -1 : 1));
     }
-
+    
     useEffect(() => {
         var districtStats = [];
         for (let i = 0; i < boxes.length; i++) {
@@ -41,7 +51,16 @@ const BoxAndWhisker = (props) => {
         }
         setEnsemble(districtStats);
         console.log(ensemble);
+        getEnactedData();
     }, [basis, boxes]);
+
+    const handleBasisSelect = (key) => {
+        setBasis(basisMap[key]);
+        setBoxes(props.plots.filter(single => single.basis == key).sort((a, b) => (a.max > b.max) ? 1 : -1));
+        console.log(boxes);
+    }
+
+    
 
     function getData(data,k){
         let dataPoints = [];
@@ -71,15 +90,15 @@ const BoxAndWhisker = (props) => {
             type : "boxAndWhisker",
             dataPoints : ensemble
             },
-            // {
-            //     type: "scatter",
-            //     name: "Enacted",
-            //     color: "steelblue",
-            //     markerType: "circle",
-            //     toolTipContent: "<span style=\"color:steelblue\">{labelPoints}</span>: {y}",
-            //     showInLegend: true,
-            //     dataPoints: getData([0.1, 0.1, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07], "Enacted")
-            // },
+            {
+                type: "scatter",
+                name: "Enacted",
+                color: "steelblue",
+                markerType: "circle",
+                toolTipContent: "<span style=\"color:steelblue\">{labelPoints}</span>: {y}",
+                showInLegend: true,
+                dataPoints: getData(enactedData, "Enacted")
+            },
         //     {
         //         type: "scatter",
         //         name: "Proposed",
